5519e60459f2866b0fc1314d1dc23d051b483dd5
Makeda P Mckune <makedamckune@gmail.com>
2019-09-30 02:18:42 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,23 @@
-def addition(num1, num2)
-
+def addition(5,4)
+  puts (num1 + num2)
 end
 
-def subtraction(num1, num2)
-
+def subtraction(10,5)
+  puts (num1 - num2)
 end
 
-def division(num1, num2)
-
+def division(50,2)
+  puts (num1 / num2)
 end
 
-def multiplication(num1, num2)
-
+def multiplication(4,30)
+  puts (num1 * num2)
 end
 
-def modulo(num1, num2)
-
+def modulo(34,5)
+  puts (num1 % num2)
 end
 
 def square_root(num)
-
+  put ( Math.sqrt(num))
 end
